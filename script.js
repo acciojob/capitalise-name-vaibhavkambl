d14c365e1e0bf024d727194009a8eeb96b4d2305
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-20 15:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function convertToUpperCase() {
 	var inputField = document.getElementById('fname');
 	inputField.value = inputField.value.toUpperCase();
 }
+body: "() => { 
+  cy.visit(baseUrl); 
+  cy.get('#fname').type('jOhN dOe').blur(); 
+  cy.get('#fname').should('have.value', 'JOHN DOE'); 
+}"
