e005cba02e1b622d822a3380319e832a0a28ae35
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-20 16:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function convertToUpperCase() {
 	var inputField = document.getElementById('fname');
 	inputField.value = inputField.value.toUpperCase();
 }
+body: "() => { 
+  cy.visit(baseUrl); 
+  cy.get('#fname').type('jOhN dOe').blur(); 
+  cy.focused().should('have.id', 'fname'); // Ensure the input field is still focused
+  cy.get('#fname').should('have.value', 'JOHN DOE'); 
+}",
