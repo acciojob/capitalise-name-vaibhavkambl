370a19f4c46a931e8b840e730bf778a698a6581b
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-20 16:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //your JS code here. If required.
-body: "() => { 
-  cy.visit(baseUrl); 
-  cy.get('#fname').type('jOhN dOe').blur(); 
-  cy.get('#fname').should(($input) => {
-    expect($input.val()).to.equal('JOHN DOE');
-  }); 
-}"
+function convertToUpperCase() {
+	var inputField = document.getElementById('fname');
+	inputField.value = inputField.value.toUpperCase();
+}
